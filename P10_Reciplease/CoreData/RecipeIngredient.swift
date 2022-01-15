bd94abd1c5ea38ee32fb3ec3e96aaab8a267990d
Sebastien Gaillard <gaillard.sebastien@outlook.fr>
2022-01-15 17:32:55 +0100

    Added RecipeIngredient entity

---
+++ P10_Reciplease/CoreData/RecipeIngredient.swift
@@ -0,0 +1,11 @@
+//
+//  RecipeIngredient.swift
+//  P10_Reciplease
+//
+//  Created by Sebastien Gaillard on 15/01/2022.
+//
+
+import Foundation
+import CoreData
+
+class RecipeIngredient: NSManagedObject {}
